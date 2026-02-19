4aaee1093b9ef947490896d94c185eb2f7611ea0
cebrano5 <alhamed.cebrano@urios.edu.ph>
2026-02-19 12:03:25 +0800

    test

---
+++ WebDev_Proj/script.js
@@ -0,0 +1,21 @@
+var aboutBtn = document.getElementById("aboutBtn");
+if(aboutBtn){
+  aboutBtn.onclick = function() {
+    window.location.href = "aboutme.html"; 
+  }
+}
+
+var homeBtn = document.getElementById("homeBtn");
+if(homeBtn){
+  homeBtn.onclick = function() {
+    window.location.href = "index.html"; 
+  }
+}
+
+
+var formBtn = document.getElementById("formBtn");
+if(formBtn){
+  formBtn.onclick = function() {
+    window.location.href = "userform.html";
+  }
+}
